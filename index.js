7376a1c7a9d74a3438cb30270efcf845f0eb3029
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-16 17:41:09 -0300

    config fixed

--- index.js
+++ index.js
@@ -1,133 +1,144 @@
 module.exports = function (data) {
 
-    // Lodash Module
-    const _ = require('lodash');
+    // Exist Data
+    if (data) {
 
-    // Config
-    const booruCfg = _.defaultsDeep({}, data.config, {
-        version: 1,
-        id: '',
-        name: '',
-        url: '',
-        tagListVar: 'tags',
-        idVar: 'id',
-    });
+        // Lodash Module
+        const _ = require('lodash');
 
-    // Config
-    const tinyCfg = _.defaultsDeep({}, data.config, {
-        key: '',
-        timeout: 10,
-        pages: 1,
-        query: '*',
-        filter_id: 56027,
-        per_page: 50
-    });
+        // Config
+        const booruCfg = _.defaultsDeep({}, data.booru, {
+            version: 1,
+            id: '',
+            name: '',
+            url: '',
+            tagListVar: 'tags',
+            idVar: 'id',
+        });
 
-    // Config Key
-    if (
-        (typeof tinyCfg.key === "string" && tinyCfg.key.length > 0) ||
-        (typeof tinyCfg.key === "number" && !isNaN(tinyCfg.key))
-    ) {
-        tinyCfg.key = '&key=' + encodeURIComponent(tinyCfg.key);
-    } else {
-        tinyCfg.key = '';
-    }
+        // Config
+        const tinyCfg = _.defaultsDeep({}, data.config, {
+            key: '',
+            timeout: 10,
+            pages: 1,
+            query: '*',
+            filter_id: 56027,
+            per_page: 50
+        });
 
-    // Create Settings
-    const booru_settings = _.defaultsDeep({}, data.module, {
+        // Config Key
+        if (
+            (typeof tinyCfg.key === "string" && tinyCfg.key.length > 0) ||
+            (typeof tinyCfg.key === "number" && !isNaN(tinyCfg.key))
+        ) {
+            tinyCfg.key = '&key=' + encodeURIComponent(tinyCfg.key);
+        } else {
+            tinyCfg.key = '';
+        }
 
-        // ID
-        id: booruCfg.id,
+        // Create Settings
+        const booru_settings = _.defaultsDeep({}, data.module, {
 
-        // DB
-        db: {
-            type: 'ref',
-            data: null
-        },
+            // ID
+            id: booruCfg.id,
 
-        // Byte Limit
-        byteLimit: {
+            // DB
+            db: {
+                type: 'ref',
+                data: null
+            },
 
-            // JSON
-            json: {
+            // Byte Limit
+            byteLimit: {
 
-                // Tag
-                tag: 1048576,
+                // JSON
+                json: {
 
-                // Error
-                error: 1048576
+                    // Tag
+                    tag: 1048576,
+
+                    // Error
+                    error: 1048576
+
+                }
 
             }
+        });
 
+        // Main Config
+        const mainConfig = {
+            name: booruCfg.name,
+            url: booruCfg.url,
+            module_name: booruCfg.id + '_http_api_v' + booruCfg.version,
+            tagListVar: booruCfg.tagListVar,
+            idVar: booruCfg.idVar,
+        };
+
+        // Config Fusion
+        for (const item in mainConfig) {
+            booru_settings[item] = mainConfig[item];
         }
-    });
 
-    // Main Config
-    const mainConfig = {
-        name: booruCfg.name,
-        url: booruCfg.url,
-        module_name: booruCfg.id + '_http_api_v' + booruCfg.version,
-        tagListVar: booruCfg.tagListVar,
-        idVar: booruCfg.idVar,
-    };
+        // Booru
+        const booruDatabase = require('@tinypudding/firebase-booru-database');
 
-    // Config Fusion
-    for (const item in mainConfig) {
-        booru_settings[item] = mainConfig[item];
-    }
+        // Philomena
+        const philomena = new booruDatabase(booru_settings);
 
-    // Booru
-    const booruDatabase = require('@tinypudding/firebase-booru-database');
+        // Prepare Module
+        const items_to_use = {};
 
-    // Philomena
-    const philomena = new booruDatabase(booru_settings);
+        // Run Code
+        items_to_use.run = function () {
+            return new Promise(async function (resolve, reject) {
 
-    // Prepare Module
-    const items_to_use = {};
+                // Try Get
+                try {
 
-    // Run Code
-    items_to_use.run = function () {
-        return new Promise(async function (resolve, reject) {
+                    // Get Errors
+                    const errorResult = await philomena.checkError();
 
-            // Try Get
-            try {
+                    // Check Timeout
+                    if (!errorResult.error) {
 
-                // Get Errors
-                const errorResult = await philomena.checkError();
+                        // Fetch
+                        const fetch = require('node-fetch');
 
-                // Check Timeout
-                if (!errorResult.error) {
+                        // Image List
+                        const image_list = [];
+                        let total_images = 0;
 
-                    // Fetch
-                    const fetch = require('node-fetch');
+                        await require('for-promise')({ data: tinyCfg.pages }, function (item, fn, fn_error) {
 
-                    // Image List
-                    const image_list = [];
-                    let total_images = 0;
+                            // Page
+                            const page = item + 1;
 
-                    await require('for-promise')({ data: tinyCfg.pages }, function (item, fn, fn_error) {
+                            // Response
+                            fetch(`${mainConfig.url}/api/v${booruCfg.version}/json/search/images?q=${encodeURIComponent(tinyCfg.query)}&filter_id=${encodeURIComponent(tinyCfg.filter_id)}&page=${String(page)}&per_page=${encodeURIComponent(tinyCfg.per_page)}${tinyCfg.key}`).then(response => {
 
-                        // Page
-                        const page = item + 1;
+                                // Search Items
+                                response.json().then(result => {
 
-                        // Response
-                        fetch(`${mainConfig.url}/api/v${booruCfg.version}/json/search/images?q=${encodeURIComponent(tinyCfg.query)}&filter_id=${encodeURIComponent(tinyCfg.filter_id)}&page=${String(page)}&per_page=${encodeURIComponent(tinyCfg.per_page)}${tinyCfg.key}`).then(response => {
+                                    // Get Total Result
+                                    total_images = result.total;
 
-                            // Search Items
-                            response.json().then(result => {
-
-                                // Get Total Result
-                                total_images = result.total;
-
-                                // Check Results
-                                if (Array.isArray(result.images) && result.images.length > 0 && typeof result.total === "number" && result.total > 0) {
-                                    for (const image in result.images) {
-                                        image_list.push(result.images[image]);
+                                    // Check Results
+                                    if (Array.isArray(result.images) && result.images.length > 0 && typeof result.total === "number" && result.total > 0) {
+                                        for (const image in result.images) {
+                                            image_list.push(result.images[image]);
+                                        }
                                     }
-                                }
+
+                                    // Complete
+                                    fn();
+                                    return;
+
+                                }).catch(err => {
+                                    fn_error(err);
+                                    return;
+                                });
 
                                 // Complete
-                                fn();
                                 return;
 
                             }).catch(err => {
@@ -138,63 +149,60 @@ module.exports = function (data) {
                             // Complete
                             return;
 
-                        }).catch(err => {
-                            fn_error(err);
-                            return;
                         });
 
-                        // Complete
-                        return;
+                        // Exist Data
+                        if (Array.isArray(image_list) && image_list.length > 0 && typeof total_images === "number" && total_images > 0) {
+                            await philomena.getDBItem('itemTotal').set(total_images)
+                            errorResult.data = await philomena.updateDatabase(image_list);
+                        }
 
-                    });
+                    }
 
-                    // Exist Data
-                    if (Array.isArray(image_list) && image_list.length > 0 && typeof total_images === "number" && total_images > 0) {
-                        await philomena.getDBItem('itemTotal').set(total_images)
-                        errorResult.data = await philomena.updateDatabase(image_list);
+                    // Nope
+                    else {
+                        errorResult.data.timeout--;
+                        await philomena.setErrorTimeout(errorResult.data.timeout);
+                    }
+
+                    // Send Result
+                    resolve(errorResult);
+
+                }
+
+                // Error
+                catch (err) {
+
+                    // Send Error
+                    try {
+                        await philomena.error({ message: err.message, timeout: tinyCfg.timeout });
+                        reject(err);
+                    }
+
+                    // Error Again
+                    catch (err2) {
+                        reject(err2);
                     }
 
                 }
 
-                // Nope
-                else {
-                    errorResult.data.timeout--;
-                    await philomena.setErrorTimeout(errorResult.data.timeout);
-                }
+                // Complete
+                return;
 
-                // Send Result
-                resolve(errorResult);
+            });
+        };
 
-            }
+        // Get Object
+        items_to_use.getRoot = function () {
+            return philomena;
+        };
 
-            // Error
-            catch (err) {
+        // Complete
+        return items_to_use;
 
-                // Send Error
-                try {
-                    await philomena.error({ message: err.message, timeout: tinyCfg.timeout });
-                    reject(err);
-                }
+    }
 
-                // Error Again
-                catch (err2) {
-                    reject(err2);
-                }
-
-            }
-
-            // Complete
-            return;
-
-        });
-    };
-
-    // Get Object
-    items_to_use.getRoot = function () {
-        return philomena;
-    };
-
-    // Complete
-    return items_to_use;
+    // Nope
+    else { throw new Error('The data value is empty!'); }
 
 };
